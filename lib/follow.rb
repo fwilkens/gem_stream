4da46a99e869505fa0791fee2224a959d8aef07a
Fletcher Wilkens <fletcher.wilkens1@gmail.com>
2019-06-02 15:48:59 -0700

    cleanup api timeframe handling, version/synced events

--- lib/follow.rb
+++ lib/follow.rb
@@ -4,31 +4,57 @@ require 'net/http'
 
 module Follow
   class Follower
-    ONE_MINUTE = 60
+    SYNCED_IF_WITHIN_SECONDS_OF_PRESENT = 10
     ONE_DAY = 86400
-    SLEEP_INTERVAL = 3
+    # In theory, can be .1 (or maybe less since we're processing between reqs)
+    # https://guides.rubygems.org/rubygems-org-api/#rate-limits
+    DEFAULT_RUBYGEMS_API_INTERVAL = 1
     SLEEP_INTERVAL_WHEN_UP_TO_DATE = ONE_MINUTE * 3
     RUBYGEMS_ENDPOINT = 'https://rubygems.org/api/v1/timeframe_versions.json'.freeze
     MAX_RUBY_GEMS_QUERY_RANGE_IN_SECONDS = 6 * ONE_DAY # It's actually 7 days, but use 6 to be safe
 
+    class_attribute :_on_version
+    class_attribute :_on_synced
+
+    def self.on_version=(proc)
+      raise ArgumentError if !proc.is_a?(Proc)
+      self._on_version = proc
+    end
+
+    def self.on_synced=(proc)
+      raise ArgumentError if !proc.is_a?(Proc)
+      self._on_synced = proc
+    end
+
     def initialize(start_from_time:)
-      @start_from_time = start_from_time
+      @synced_up_to_time = start_from_time
     end
 
     def run
-      @synced_up_to_time = @start_from_time || last_published_at_in_code_recon
-
-      poll_rubygems_api
+      follow
     end
 
-    def poll_rubygems_api
-      puts 'getting latest from rubygems...'
-      get_latest_from_rubygems_for_current_period
+    private
 
-      poll_rubygems_api
+    def follow
+      query_rubygems
+
+      if synced?
+        synced
+      else
+        increment_query_window && follow
+      end
     end
 
-    def get_latest_from_rubygems_for_current_period(page: 1)
+    def synced?
+      (Time.now - @synced_up_to_time) <= SYNCED_IF_WITHIN_SECONDS_OF_PRESENT
+    end
+
+    def synced
+      self.class._on_synced.call()
+    end
+
+    def query_rubygems(page: 1)
       params = {
           from: @synced_up_to_time.iso8601,
           to: (@synced_up_to_time + MAX_RUBY_GEMS_QUERY_RANGE_IN_SECONDS).iso8601,
@@ -40,6 +66,7 @@ module Follow
       response = Net:HTTP.get_response(RUBYGEMS_ENDPOINT, options)
 
       # TODO: something smarter here
+      # Allow config option for handling api errors?
       if response.code != '200'
         puts "Got status #{response.code} from rubygems for #{RUBYGEMS_ENDPOINT} with options: #{params.inspect}"
         return
@@ -47,41 +74,21 @@ module Follow
 
       versions = JSON.parse(response.body)
 
-      return increment_period_or_up_to_date if versions.size == 0
+      return if versions.size == 0
 
       versions.each do |version|
-        # TODO -- call the specified on_version hook
+        self.class._on_version.call(version)
       end
 
-      sleep SLEEP_INTERVAL # don't spam rubygems
+      sleep DEFAULT_RUBYGEMS_API_INTERVAL
       get_latest_from_rubygems_for_current_period(page: page+1)
     end
 
-    def increment_period_or_up_to_date
-      if @synced_up_to_time >= one_minute_ago # TODO: do something better
-        up_to_date
-      else
-        increment_current_period
-        get_latest_from_rubygems_for_current_period
-      end
-    end
-
-    def up_to_date
-      print 'up to date, sleeping...'
-      @synced_up_to_time = Time.now - ONE_MINUTE
-
-      sleep SLEEP_INTERVAL_WHEN_UP_TO_DATE
-    end
-
-    def increment_current_period
+    def increment_query_window
       @synced_up_to_time = [
         (@synced_up_to_time + MAX_RUBY_GEMS_QUERY_RANGE_IN_SECONDS),
         Time.now
       ].min
-    end
-
-    def one_minute_ago
-      Time.now - (1 * ONE_MINUTE)
     end
   end
 end
